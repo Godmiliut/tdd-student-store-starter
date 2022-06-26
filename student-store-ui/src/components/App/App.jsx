f893f55fd29bfdbd6afb5b711874fb4b0f83824c
Gabriel Bremo <91387413+Godmiliut@users.noreply.github.com>
2022-06-26 16:47:58 -0400

    Add files via upload

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,3 +1,4 @@
+//  Import necessary components and libraries
 import * as React from "react"
 import axios from "axios"
 import { useEffect } from "react"
@@ -11,7 +12,9 @@ import Hero from "../Hero/Hero"
 import SubNavbar from "../SubNavbar/SubNavbar"
 import "./App.css"
 
+//  General app component
 export default function App() {
+  //  Declare state variables
   const [products,setProducts] = React.useState([]);
   const [isFetching, setIsFetching] = React.useState("");
   const [error, setError] = React.useState("");
@@ -23,6 +26,7 @@ export default function App() {
   const [success, setSuccess] = React.useState(false);
   const [receipt, setReceipt] = React.useState({});
 
+  //  Every time the component is mounted or updated, fetch the products from the API
   React.useEffect(() =>{
     axios.get("http://localhost:3001/store/").then(
       (response) =>{
@@ -36,6 +40,7 @@ export default function App() {
     )
     }, [])
 
+  // Handles toggling the width of the Sidebar component 
   function handleOnToggle(){
     if(isOpen==""){
       setIsOpen(false);
@@ -43,11 +48,14 @@ export default function App() {
     setIsOpen(!isOpen);
   }
 
+  //  Handles the event of adding an item to the shoppingCart state variable
+  //  @productId ID of the product to be added to the shoppingCart state variable
   function handleAddItemToCart(productId){
     let shoppingProducts=[...shoppingCart];
     let alreadyThere= false;
     if(shoppingProducts.length > 0){
       let i = 0;
+      //  Iterate through the shoppingCart array while the product has not been found
       while(i < shoppingProducts.length && alreadyThere == false){
         if(shoppingProducts[i].itemId == productId){
           let newQty = shoppingProducts[i].quantity + 1;
@@ -61,6 +69,7 @@ export default function App() {
         i++;
       }
     }
+    //  If the product is never found, add it to the array
     if(alreadyThere == false){
       let shoppingProduct=""
       shoppingProduct={
@@ -71,6 +80,8 @@ export default function App() {
     }
   }
   
+  //  Handles the event of removing an item from the shoppingCart state variable
+  //  @@productId ID of the product to be removed
   function handleRemoveItemFromCart(productId){
     let shoppingProducts=[...shoppingCart];
     if(shoppingProducts.length > 0){
@@ -82,6 +93,7 @@ export default function App() {
             itemId: productId,
             quantity: newQty
           }
+          //  If there is only one of that item in the cart, remove it completely from @shoppingCart
           if(shoppingProducts[i].quantity < 1){
             shoppingProducts.splice(i,1);
           }
@@ -92,13 +104,16 @@ export default function App() {
     }
   }
 
+  //  Handles the change of an item inside the @checkoutForm state variable
+  //  @@name key of the item to be changed
+  //  @@value value fo the item to be changed
   function handleOnCheckoutFormChange(name, value){
     setCheckoutForm({...checkoutForm, [name]: value})
   }
 
+  //  Handles the post request of the @checkoutForm to the API
   async function handleOnSubmitCheckoutForm(){
-    console.log(checkoutForm);
-    console.log(shoppingCart);
+    //  Pass the user information and the shoppingcart as the request's body
     axios.post("http://localhost:3001/store/",{user: checkoutForm, shoppingCart: shoppingCart}).then(
       (response) =>{
         setReceipt(response.data.purchase.receipt);
